5539277ae0b822ab7330ef8b1113bd7a157429c1
vadimskyi <gdvadimskyi@gmail.com>
2022-05-14 10:32:07 +0300

    Initial commit

---
+++ Assets/Scripts/ScrollViewController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ScrollViewController : MonoBehaviour
+{
+    [SerializeField] private int _listSize;
+    [SerializeField] private RectTransform _content;
+    [SerializeField] private RectTransform _listItemPrefab;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    public void On_GenerateClick()
+    {
+        for (int i = 0; i < _content.childCount; i++)
+        {
+            Destroy(_content.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < _listSize; i++)
+        {
+            Instantiate(_listItemPrefab, _content);
+        }
+    }
+}
